0e7b642b2cfec2326368b573544176a4011da12d
IgorAlvaredo <94122674+IgorAlvaredo@users.noreply.github.com>
2023-10-11 01:00:26 -0300

    update

---
+++ src/components/user/Sidebar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Sidebar() {
+    return (
+      <nav>
+        <ul>
+          <li><Link href={""}/></li>
+        </ul>
+      </nav>
+    )
+  }
+  
